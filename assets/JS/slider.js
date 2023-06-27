e77a7892d0850c5ff49fe48b5128044a0c78e14b
Titou360 <clement.felices@gmail.com>
2023-06-27 15:59:03 +0200

    ok 2706 à 15:58

--- assets/JS/slider.js
+++ assets/JS/slider.js
@@ -1,4 +1,4 @@
-const slides = [ /*Constance tableau */
+const slides = [
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
@@ -17,70 +17,86 @@ const slides = [ /*Constance tableau */
 	}
 ]
 
+// Create the banner with JS 
+document.addEventListener('DOMContentLoaded', function() {
+	var bannerDiv = document.querySelector('#banner');
+	var currentIndex =0;
+	
+  
+	var arrowLeftImg = document.createElement('img');
+		arrowLeftImg.classList.add('arrow', 'arrow_left');
+		arrowLeftImg.src = './assets/images/arrow_left.png';
+
+		arrowLeftImg.addEventListener('click', function() {
+			currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+			updateBanner();
+		  });
 
 
+	var bannerImg = document.createElement('img');
+		bannerImg.className = 'banner-img';
+		bannerImg.src = './assets/images/slideshow/' + slides[currentIndex].image;
 
 
+	var bannerTagline = document.createElement('p');
+		bannerTagline.innerHTML = slides[currentIndex].tagLine;
 
 
-const imageElement = document.querySelector(".banner-img");
-const taglineElement = document.querySelector("p");
-let currentIndex = 0;
+	var arrowRightImg = document.createElement('img');
+		arrowRightImg.classList.add('arrow', 'arrow_right');
+		arrowRightImg.src = './assets/images/arrow_right.png';
 
-const leftArrow = document.querySelector(".arrow_left");
-leftArrow.addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    const currentSlide = slides[currentIndex];
-    /*imageElement.src = "assets/images/slideshow/" + currentSlide.image;
-    taglineElement.innerHTML = currentSlide.tagLine;*/
-    setCurrentSlide(currentIndex);
-});
-
-const rightArrow = document.querySelector(".arrow_right");
-rightArrow.addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % slides.length;
-    const currentSlide = slides[currentIndex];
-    /*imageElement.src = "assets/images/slideshow/" + currentSlide.image;
-    /*taglineElement.innerHTML = currentSlide.tagLine;*/
-    setCurrentSlide(currentIndex);
-});
-
-/// J'ajoute les bullet points sur la partie basse du Slider (1 point par image)
-	// Je peux vérifier le nombre de ligne de contenu dans mon tableau 
-console.log("Nombre de lignes dans le tableau :" + slides.length); // /!\ à effacer avant mise en ligne
-
-	// Je crée la constante pour relier la classe .dots
-const dotsContainer = document.querySelector('.dots');
-const dots = [];
-	// Je génére les "bullet points" pour chaque ligne du tableau
-	//forEach permet d'éxecuter une fonction donnée sur chaque élément du tableau slides []
-slides.forEach((_slide, index) => {
-const dot = document.createElement('span');
-	dot.classList.add('dot');
-	/**** Pour rendre cliquable les dots ****/
-	/*dot.addEventListener('click', () => {
-		setCurrentSlide(index);
-	});*/
-	dotsContainer.appendChild(dot);
-	dots.push(dot);
-});
+		arrowRightImg.addEventListener('click', function() {
+			currentIndex = (currentIndex + 1) % slides.length;
+			updateBanner();
+			});
 
 
-
-	// Je crée la fonction qui me permet que le "bullet-point" prenne ou non le CSS
-function setCurrentSlide(currentIndex) {
-	const currentSlide = slides[currentIndex];
-	imageElement.src = "assets/images/slideshow/" + currentSlide.image;
-	taglineElement.innerHTML = currentSlide.tagLine;
-	const dots = document.querySelectorAll('.dot');
-
-	dots.forEach((dot, i) => {
-		dot.classList.toggle('dot_selected', i===currentIndex)
-	});
-}
-
-	// Appeler setCurrentSlide() avec l'index initial
-setCurrentSlide(0);
+	var dotsImg = document.createElement('div');
+		dotsImg.className = 'dots';
 
 
+	function updateBanner () {
+		bannerImg.src = './assets/images/slideshow/' + slides[currentIndex].image;
+
+		if (bannerTagline) {
+			bannerDiv.removeChild(bannerTagline);
+		}
+
+		bannerTagline = document.createElement('p');
+		bannerTagline.innerHTML = slides[currentIndex].tagLine;
+
+		bannerDiv.appendChild(bannerTagline);
+
+
+		dots.forEach((dot, i) => {
+			dot.classList.toggle('dot_selected', i === currentIndex);
+		  });
+
+	}
+
+	// Add the elements to the banner
+	bannerDiv.appendChild(arrowLeftImg);
+	bannerDiv.appendChild(bannerImg);
+	bannerDiv.appendChild(bannerTagline);
+	bannerDiv.appendChild(arrowRightImg);
+	bannerDiv.appendChild(dotsImg);
+
+	
+	// Add the dots to the banner
+	var dots = [];
+  		slides.forEach((_slide, index) => {
+    var dot = document.createElement('span');
+    	dot.classList.add('dot');
+    	dot.addEventListener('click', function() {
+      		currentIndex = index;
+      		updateBanner();
+    	});
+    	
+		dotsImg.appendChild(dot);
+    	dots.push(dot);
+  	});
+
+  updateBanner();
+  });
 
